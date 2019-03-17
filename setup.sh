4d2d5fd4a9822340891cfb63fdab6713361a21d3
John Gathure <juangathure@gmail.com>
2019-03-17 14:10:11 +0300

    Bash script to setup conf files for vim and tmux.

---
+++ setup.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# color formats
+RED="`tput setaf 1`"
+GREEN="`tput setaf 2`"
+BOLD="`tput bold`"
+NORM="`tput sgr0`"
+
+# User's home dir
+HOMEDIR=$HOME
+
+clear
+echo -e "Setting up vim/nvim and tmux config files..... \n"
+# Promt user to enter editor they are using.
+read -p "${BOLD}Are you using vim or neovim (VI) for vim (NVI) for neovim... ${NORM}" editor
+
+# enable case insensitive matching ie if user enters vi create sys link
+shopt -s nocasematch
+
+if [ -z $editor ]
+then
+    echo "${RED}Please enter editor...${NORM}"
+    exit 1
+elif [[ $editor == "VI" ]]
+then
+    echo "${BOLD}Creating system link $(pwd)/vimrc -> $HOMEDIR/.vimrc${NORM}"
+    ln -sf $(pwd)/vimrc $HOMEDIR/.vimrc
+    echo "${GREEN}Successfully created system link${NORM}"
+elif [[ $editor == "NVI" ]]
+then 
+
+    echo "${BOLD}Creating system link $(pwd)/vimrc -> ${HOMEDIR}/.config/nvim/init.vim${NORM}"
+    [ -d $HOMEDIR/.config/nvim ] || mkdir -p $HOMEDIR/.config/nvim
+    ln -sf $(pwd)/vimrc $HOMEDIR/.config/nvim/init.vim
+    echo "${GREEN}Successfully created system link${NORM}"
+else
+    echo -e "${RED}Incorrect input${NORM}\a"
+    exit 1
+fi
+# undo case insensitive match
+shopt -u nocasematch
+
+# setup tmux conf.
+echo "${BOLD}Creating system link $(pwd)/tmux.conf > ${HOMEDIR}/.tmux.conf${NORM}"
+ln -sf $(pwd)/tmux.conf ${HOMEDIR}/.tmux.conf
+echo "${GREEN}Successfully created system link${NORM}"
