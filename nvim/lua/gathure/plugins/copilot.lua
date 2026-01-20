b977c227175647a49a47cb2c599dff66165da2a5
John Gathure <juangathure@gmail.com>
2026-01-20 17:18:27 +0300

    fix copilot lazy-loading event placement

--- nvim/lua/gathure/plugins/copilot.lua
+++ nvim/lua/gathure/plugins/copilot.lua
@@ -1,12 +1,12 @@
 return {
   "zbirenbaum/copilot-cmp",
-  event = "InsertEnter",
   config = function()
     require("copilot_cmp").setup()
   end,
   dependencies = {
     "zbirenbaum/copilot.lua",
     cmd = "Copilot",
+    event = "InsertEnter",
     config = function()
       require("copilot").setup {
         suggestion = { enabled = false },
